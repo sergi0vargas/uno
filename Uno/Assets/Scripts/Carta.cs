2df75586fe55600f371d9117153ef9efbabd935d
Sergio Vargas <sergiovargas9@gmail.com>
2015-02-27 00:19:08 -0300

    Agregadas sprites cartas

--- Uno/Assets/Scripts/Carta.cs
+++ Uno/Assets/Scripts/Carta.cs
@@ -5,4 +5,8 @@ public class Carta : MonoBehaviour {
 
     public Color color = Color.gray;
     public int valor = 0;
+
+    public bool cartaSalta = false;
+    public bool cartaMasDos = false;
+
 }
